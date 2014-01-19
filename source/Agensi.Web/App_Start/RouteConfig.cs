b92fc02be51b08ac76aa23a434c374566f6368aa
Michiwaki Shohei <michiwaki.shohei@gmail.com>
2014-01-20 01:40:38 +0900

    ドキュメントルートをBoard/Indexに変更

--- source/Agensi.Web/App_Start/RouteConfig.cs
+++ source/Agensi.Web/App_Start/RouteConfig.cs
@@ -16,7 +16,7 @@ namespace Agensi.Web
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "Board", action = "Index", id = UrlParameter.Optional }
             );
         }
     }
