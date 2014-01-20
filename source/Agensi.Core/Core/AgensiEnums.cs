5661d84456c4526ea4feaacd70e505d93bdb8fb0
Michiwaki Shohei <michiwaki.shohei@gmail.com>
2014-01-21 00:53:44 +0900

    Enum追加

---
+++ source/Agensi.Core/Core/AgensiEnums.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Agensi.Core.Core
+{
+    public class AgensiEnums
+    {
+        [Flags]
+        public enum LanguageCategory
+        {
+            Japanese = 1,
+            English = 2,
+            Chinese = 4,
+        }
+
+        public enum TopicCategory
+        {
+            IT = 1,
+            Food = 2,
+        }
+
+        public enum MediaCategory
+        {
+            Text = 1,
+            Video = 2,
+        }
+    }
+}
