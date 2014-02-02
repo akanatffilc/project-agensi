32db5055c8ed425dd693ec27f9100905d6939d82
Michiwaki Shohei <michiwaki.shohei@gmail.com>
2014-02-03 01:47:22 +0900

    AskPartialModel追加

---
+++ source/Agensi.Web/Models/Board/AskPartialModel.cs
@@ -0,0 +1,23 @@
+﻿using Agensi.Core.Category;
+using Agensi.Core.Language;
+using Agensi.Data.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Agensi.Web.Models.Board
+{
+    public class AskPartialModel
+    {
+        public AskPartialModel() { }
+
+        public Query AskQuery { get { return new Query(); } }
+
+        public Tag[] Tag { get { return new Tag[0]; } }
+
+        public AgensiLanguage[] AllLanguage { get { return AgensiLanguageManager.AllLanguage; } }
+
+        public MediaCategory[] MediaCategories { get { return MediaCategoryManager.AllMediaCategory; } }
+    }
+}
