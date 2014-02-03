4e7fee274e9fae2a581c207a40e501225602bd19
s.michiwaki <s.michiwaki@gloops.com>
2014-02-03 15:14:21 +0900

    CoreのTestプロジェクトをコミット

---
+++ source/Agensi.Core.Test/UnitTest1.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Agensi.Core.Util;
+
+namespace Agensi.Core.Test
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            var str1 = "My name is michiwaki shohei . I don't speak English";
+            var str2 = "My name is shohei michiwaki shohei michiwaki shohei michiwaki　TESTSETEST  LSKJFSLDK; LKJSDf LSKJ lKSDFJLSFJ SLKJFSLKD+FJs KLS+JFl Sdf";
+
+            var result = StringUtil.Compare(3, str2, str1);
+        }
+    }
+}
--- source/Agensi.Core/Util/StringUtil.cs
+++ source/Agensi.Core/Util/StringUtil.cs
@@ -58,5 +58,39 @@ namespace Agensi.Core.Util
             //byte型配列を16進数の文字列に変換
             return BitConverter.ToString(bs).ToLower().Replace("-", "");
         }
+
+        /// <summary>
+        /// N-gram方式で文字列の比較を行い、重複箇所の割合を求める
+        /// </summary>
+        /// <param name="strA"></param>
+        /// <param name="strB"></param>
+        /// <returns></returns>
+        public static float Compare(string strA, string strB)
+        {
+            //デフォルトはBigram
+            return Compare(2, strA, strB);
+        }
+        public static float Compare(int n, string strA, string strB)
+        {
+            // strAのN-gramリストを作成する
+            var blist = new HashSet<string>();
+            for (int i = 0; i < strA.Length - (n - 1); i++)
+            {
+                string ngitem = strA.Substring(i, n);
+                if (!blist.Contains(ngitem)) { blist.Add(ngitem); }
+            }
+            //blist.Sort();
+
+            // strBのN-gramリストとの一致を見る
+            int found = 0;
+            for (int i = 0; i < strB.Length - (n - 1); i++)
+            {
+                string ngitem = strB.Substring(i, n);
+                if (blist.Contains(ngitem)) { found++; }
+            }
+
+            // 重複箇所の割合を返す
+            return (float)found / blist.Count;
+        }
     }
 }
