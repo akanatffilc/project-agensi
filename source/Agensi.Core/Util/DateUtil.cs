3b8e892f451a389475a3b9e0af0497a4c739aba6
akanatffilc <tanaka.cliff@gmail.com>
2014-02-03 22:26:35 +0900

    コミット

--- source/Agensi.Core/Util/DateUtil.cs
+++ source/Agensi.Core/Util/DateUtil.cs
@@ -10,16 +10,27 @@ namespace Agensi.Core.Util
     {
         public static string FormatDate(DateTime now, DateTime fromToDate)
         {
-            if(now > fromToDate)
+            var timeDiffString = "";
+            var timeDiff = now - fromToDate;
+            var seconds = (int)timeDiff.TotalSeconds;
+            if (seconds < 60)
             {
-                var timeSpan = now - fromToDate;
-                return string.Format("{0}前", timeSpan);
+                timeDiffString = string.Format("{0}秒前", seconds);
+            }
+            else if (seconds < 60 * 60)
+            {
+                timeDiffString = string.Format("{0}分前", (int)Math.Ceiling(seconds / 60.0));
+            }
+            else if (seconds < 60 * 60 * 24 * 7)
+            {
+                timeDiffString = string.Format("{0}日前", (int)Math.Ceiling(seconds / (60.0 * 60.0 * 24)));
             }
             else
             {
-                var timeSpan = fromToDate - now;
-                return string.Format("{0}後", timeSpan);
+                timeDiffString = string.Format(@"{0: M/d H\:mm}", fromToDate);
             }
+
+            return timeDiffString;
         }
     }
 }
